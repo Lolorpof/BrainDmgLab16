9ae8900667d91ef4705a7a0eb9596cc3d4bc9f0d
Lolorpof <sirawit_sir@cmu.ac.th>
2023-02-07 16:14:34 +0700

    lab16_4 completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,32 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a, int &b, int &c, int &d){
+	int arr[4] = {a, b, c, d};
+	bool check[4] = {0, 0, 0, 0};
+	int count = 1;
+	while(check[0] == 0 || check[1] == 0 || check[2] == 0 || check[3] == 0){
+		int i = rand() % 4 + 1;
+		if(check[i-1] == 0){
+			if(count == 1){
+				a = arr[i-1];
+				check[i-1] = 1;
+			}
+			if(count == 2){
+				b = arr[i-1];
+				check[i-1] = 1;
+			}
+			if(count == 3){
+				c = arr[i-1];
+				check[i-1] = 1;
+			}
+			if(count == 4){
+				d = arr[i-1];
+				check[i-1] = 1;
+			}
+			count++;
+		}
+		
+	}
+}
